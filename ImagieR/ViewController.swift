82a5da2e1b92790fa001e1fdf9b293c372ed437c
QL31 <qinglian.li1070@gmail.com>
2020-05-03 18:17:22 +0200

    Implant training model

--- ImagieR/ViewController.swift
+++ ImagieR/ViewController.swift
@@ -27,11 +27,42 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let userPickedImage = info[.originalImage] as? UIImage else {fatalError("Error taking image")}
         imageTaken.image = userPickedImage
+            guard let ciImage = CIImage(image: userPickedImage) else {fatalError("Could not convert to CIImage")}
+        
+            detect(image: ciImage)
         
         imagePiker.dismiss(animated: true, completion: nil)
         
     }
 
+    func detect(image:CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: MobileNetV2().model) else{fatalError("Loading core ML model failed")}
+       
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to processe image")
+            }
+            print(result)
+            
+            if let title = result.first?.identifier {
+                
+                print("presition =\(result.first?.confidence)")
+                
+                self.navigationItem.title = title
+            }
+            
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do{
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+        
+        
+    }
     @IBAction func image(_ sender: UIBarButtonItem) {
         
         present(imagePiker, animated: true, completion: nil)

21489e770e64eccd1b845b130b658d436abc8e36
QL31 <qinglian.li1070@gmail.com>
2020-05-03 16:55:49 +0200

    Import the image recognition and enable camera data

--- ImagieR/ViewController.swift
+++ ImagieR/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageTaken: UIImageView!
+    
+    let imagePiker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePiker.delegate = self
+        imagePiker.sourceType = .photoLibrary
+        imagePiker.allowsEditing = false
+        
         // Do any additional setup after loading the view.
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let userPickedImage = info[.originalImage] as? UIImage else {fatalError("Error taking image")}
+        imageTaken.image = userPickedImage
+        
+        imagePiker.dismiss(animated: true, completion: nil)
+        
+    }
 
-
+    @IBAction func image(_ sender: UIBarButtonItem) {
+        
+        present(imagePiker, animated: true, completion: nil)
+        
+        
+    }
+    
 }
 

de9a8cdc78fbb51632b7a5702986861151e8f256
QL31 <qinglian.li1070@gmail.com>
2020-05-03 16:16:10 +0200

    Initial Commit

---
+++ ImagieR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImagieR
+//
+//  Created by li qinglian on 03/05/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
